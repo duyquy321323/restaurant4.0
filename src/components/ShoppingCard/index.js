09cef4827d8f03f848df4bd2cd018ee2b35e99ca
Tran Hung Quoc <quoc911ndu1819@gmail.com>
2024-11-02 02:16:54 +0700

    menu_idea

--- src/components/ShoppingCard/index.js
+++ src/components/ShoppingCard/index.js
@@ -1,9 +1,31 @@
 import "./ShoppingCard.css";
-
+import { useLocation } from "react-router-dom";
+import TrashIcon from "../../assets/icon/Trash-orange-black.svg"
 function ShoppingCard() {
+  const location = useLocation();
+
   return (
     <>
-      <div className="container-shopping-card">ShoppingCard</div>
+      <div className="container-shopping-card">
+          <div className="container">
+            <div className="header">
+              <h1 className="title">
+                Orders
+              </h1>
+              <div className="text">Items</div>
+              <div className="text">Qty</div>
+              <div className="text">Price</div>
+              <div className="divider"></div>
+            </div>
+            <div className="content">
+            
+            </div>
+            <div className="footer">
+            
+            </div>
+            
+          </div>
+      </div>
     </>
   );
 }
